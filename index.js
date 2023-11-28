203bb7a9b6ab93dcd32b8a1cbb3d4fcde2a2b9e3
Marmik <marmik2004@icloud.com>
2023-11-28 20:51:03 +0530

    sample

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+const { Octokit } = require('@octokit/rest');
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
+const github = new Octokit({
+  // Replace with your GitHub personal access token
+  auth: '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'
+});
 
-const makeCommit = n => {
-  if (n === 0) return simpleGit().push();
+const makeCommit = async n => {
+  if (n === 0) {
+    await simpleGit().push();
+    // Create a pull request
+    await github.pulls.create({
+      owner: 'marmik',
+      repo: 'GitHub-bot',
+      head: 'master', // Replace with the actual source branch
+      base: 'develop', // Replace with the actual target branch
+      title: 'Random commit for GitHub contribution graph',
+      body: 'This commit was generated randomly to contribute to the GitHub contribution graph.'
+    });
+  }
+
   const x = getRandomInt(0, 54);
   const y = getRandomInt(0, 6);
   const DATE = moment()
@@ -14,13 +31,15 @@ const makeCommit = n => {
     .add(x, 'w')
     .add(y, 'd')
     .format();
+
   const data = {
-    date: DATE,
+    date: DATE
   };
+
   console.log(DATE);
-  jsonfile.writeFile(FILE_PATH, data, () => {
-    simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, n - 1));
-  });
+  await jsonfile.writeFile(FILE_PATH, data);
+
+  await simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, n - 1));
 };
 
 function getRandomInt(min, max) {
@@ -29,4 +48,4 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-makeCommit(100);
+makeCommit(5);
